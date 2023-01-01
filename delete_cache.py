d87c39778603057e7edbcffe8de05eefac199546
suicxyz <120996765+suicxyz@users.noreply.github.com>
2023-01-01 12:51:40 -0300

    Add file via upload
    Execute this is script as an administrator

---
+++ delete_cache.py
@@ -0,0 +1,41 @@
+import pathlib
+import shutil
+import os, ctypes
+
+def verify_admin():
+	try:
+		is_admin = os.getuid() == 0
+	except AttributeError:
+		is_admin = ctypes.windll.shell32.IsUserAnAdmin() == 1
+
+	if is_admin != 1:
+		msg_box = ctypes.windll.user32.MessageBoxW(0, "Execute o arquivo como administrador para continuar!", "Erro de permissão", 5)
+		if msg_box == 4:
+			verify_admin()
+		elif msg_box == 2:
+			exit()
+
+def delete_files_and_dirs(path):
+	for file_name in os.listdir(path):
+		file = f"{path}{file_name}"
+
+		if os.path.isdir(file):
+			print(f"Deleting folder: \"{file}\"")
+			shutil.rmtree(file, ignore_errors=True)
+
+		if os.path.isfile(file):
+			try:
+				os.remove(file)
+			except PermissionError:
+				print(f"File \"{file}\" could not be deleted.")
+				continue
+
+			print(f"Deleting file: \"{file}\"")
+
+verify_admin()
+
+drive = pathlib.Path.home().drive
+
+delete_files_and_dirs(f"{drive}/Windows/Temp/")
+delete_files_and_dirs(f"{drive}/Windows/Prefetch/")
+delete_files_and_dirs(f"{drive}/Users/vinit/AppData/Local/Temp/")
